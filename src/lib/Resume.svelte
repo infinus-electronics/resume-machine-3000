abfeef361a3b8f583ed43da4af76759bec18b1a9
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2022-09-05 23:11:32 +0100

    architecture revamp

---
+++ src/lib/Resume.svelte
@@ -0,0 +1,19 @@
+<script lang='ts'>
+  const name = "Ye Heng Hor";
+  const phone = "+447825224575"
+
+</script>
+
+<div>
+  <div class="resume">
+    <h1>{name}</h1>
+    <h2 class="noprint">{phone}</h2>
+  </div>
+</div>
+
+<style lang="scss">
+    .resume {
+        width: 100%;
+        margin-top: 1em;
+    }
+</style>
