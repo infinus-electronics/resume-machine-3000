7eb7661e9dad0c6f80fc64f1defe70c7255553b3
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2022-09-07 23:30:23 +0100

    added some data to the yaml

--- src/lib/Resume.svelte
+++ src/lib/Resume.svelte
@@ -1,26 +1,20 @@
-<script lang='ts'>
-  const name = "Ye Heng Hor";
-  const phone = "+447825224575";
-
-  import yaml from 'js-yaml';
-  import data from '/src/me.yaml?raw';
-
+<script lang="ts">
+  import yaml from "js-yaml";
+  import data from "/src/me.yaml?raw";
 
   const resume = yaml.load(data);
-
-
+  console.log(resume)
 </script>
 
 <div>
   <div class="resume">
-    <h1>{resume['personal-information']['name']}</h1>
-    <h2 class="noprint">{phone}</h2>
+    <h1>{resume["personal-information"]["name"]}</h1>
   </div>
 </div>
 
 <style lang="scss">
-    .resume {
-        width: 100%;
-        margin-top: 1em;
-    }
+  .resume {
+    width: 100%;
+    margin-top: 1em;
+  }
 </style>
