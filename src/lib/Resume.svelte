66bc1f8d9ecc218247b650d0c992c6ee3a7f7f00
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2022-09-08 14:16:13 +0100

    got the flex to do something at least

--- src/lib/Resume.svelte
+++ src/lib/Resume.svelte
@@ -8,24 +8,47 @@
 
 <div class="resume-container">
   <div class="resume">
-    <h1>{resume["personal-information"]["name"]}</h1>
+    
+      <div class="resume-header">
+        <h1>{resume["personal-information"]["name"]}</h1>
+        <p>{resume["personal-information"]["website"]}
+      </div>
+    
   </div>
 </div>
 
 <style lang="scss">
   @import url("https://fonts.googleapis.com/css?family=Noto+Sans:wght@100...900&display=swap");
 
+  $margin: 1em;
+
   .resume-container {
     width: 70%;
     margin: auto;
+    border: solid;
   }
 
   .resume {
     width: 100%;
-    margin: 1em;
+    padding: 0;
+    margin: 0;
+    font-family: "Noto Sans";
+    display: flex;
+    
+
+    .resume-header{
+      margin: $margin;
+      padding: 0;
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      
+    }
 
     h1 {
       font-family: "Noto Sans";
+      margin: 0;
     }
   }
   @media print {
